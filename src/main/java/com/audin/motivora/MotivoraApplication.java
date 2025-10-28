7b3f629e03e3b57632c0a8c1beff2261d7381193
Audin <audinjunior1@gmail.com>
2025-10-28 12:32:52 +0100

    first commit

---
+++ src/main/java/com/audin/motivora/MotivoraApplication.java
@@ -0,0 +1,13 @@
+package com.audin.motivora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotivoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/audin/motivora/MotivoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audin.motivora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotivoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
